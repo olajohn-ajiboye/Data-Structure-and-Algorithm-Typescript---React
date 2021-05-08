381732a38719604a941f0ae974ff0a46891b003a
Ola John Ajiboye <33224920+olajohn-ajiboye@users.noreply.github.com>
2021-05-08 13:59:44 +0200

    graph(traversal): depth first search

--- src/algorithms/graph/traversal/dfs.ts
+++ src/algorithms/graph/traversal/dfs.ts
@@ -1,3 +1,12 @@
-export function dfsGraph() {
+import { AdjacencySetGraph } from './../../../data-structures/graphs/adjacency-list.graph';
 
+export function dfsGraph(graph: AdjacencySetGraph, startNode = 0, lookUp = new Map(), result: number[] = []) {
+    if (lookUp.has(startNode)) return
+    lookUp.set(startNode, true)
+    result.push(startNode)
+    const adjacents = graph.getAdjacentVertices(startNode)
+    adjacents.forEach(node => {
+        if (!lookUp.has(node)) dfsGraph(graph, node, lookUp, result)
+    })
+    return result
 }
